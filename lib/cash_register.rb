66685298a7f15052b0812e5873fc7fb59763e360
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-21 22:52:57 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   def add_item(title, price, quantity=1)
     @new_total << price * quantity
     @total = @new_total.sum
-    @@item << title * quantity
+    @item << title * quantity
   end
   
   def apply_discount
