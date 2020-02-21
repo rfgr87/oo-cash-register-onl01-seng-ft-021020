32540d7b2ff50e64282d474cf62e0617396f9a62
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-21 22:31:22 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,4 +1,16 @@
 class CashRegister
-
+  attr_accessor :total
+  
+  def initialize(discount)
+    @total = 0
+    @employee_discount = discount
+  end
+  
+  def add_item(title, price)
+    @new_total += @total + price
+  end
+  
+  def apply_discount
+    @new_total = @employee_discount * 0.01
   
 end
