8bf4c9a1bac632c7962f95f40a3c8a9a9fb73a64
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-21 23:38:41 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,7 +17,7 @@ class CashRegister
     new_total = []
     item = []
   def add_item(title=nil, price=0.0, quantity=1)
-    total += (price*quantity)
+    @total += (price*quantity)
     new_total.push(price.to_i.to_f * quantity.to_i.to_f)
     i = 0
     while i < quantity
