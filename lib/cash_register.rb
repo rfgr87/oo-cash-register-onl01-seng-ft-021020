d0b4bd66d363732084e7f14efd0fce5320c466bc
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-22 00:29:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -37,7 +37,7 @@ class CashRegister
     else
       @total = @total - (@total * (@discount/100.0))
       @total
-      return "After the discount, the total comes to $#{@total}."
+      return "After the discount, the total comes to $#{@total.to_i}."
     end
   end
   
